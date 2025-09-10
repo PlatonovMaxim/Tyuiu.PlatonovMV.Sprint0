64447be14bb51addba190e34693aa0287875f86b
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-09-10 19:25:57 +0500

    Task1

---
+++ Tyuiu.PlatonovMV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello, World!");
+string name = "Максим";
+int age = 18;
+Console.WriteLine("Здравствуйте, меня зовут " + name +  " мне " + age +  " лет! ");
+Console.ReadLine();
+//проверочка
